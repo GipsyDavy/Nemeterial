b15cac4e0b8c4ce906c0d9a9cbe3f13034fe939c
Gipsybuho <gipsybuho@gmail.com>
2026-03-04 11:49:23 +0100

    actualizar

--- spring-boot/backend-api/src/main/java/com/gipsybuho/backend_api/MiApiRestApplication.java
+++ spring-boot/backend-api/src/main/java/com/gipsybuho/backend_api/MiApiRestApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+
 public class MiApiRestApplication {
 
 	public static void main(String[] args) {
